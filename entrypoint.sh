919850b278e021ddc755dc9dd9938a6a457a5efc
mariusraupach <mariusraupach@icloud.com>
2025-12-11 15:17:51 +0100

    Add entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+INSTALL_DIR="/home/steam/Steam/steamapps/common/Project Zomboid Dedicated Server"
+
+if [ ! -f "$INSTALL_DIR/start-server.sh" ]; then
+    /home/steam/steamcmd/steamcmd.sh \
+        +login anonymous \
+        +app_update 380870 -beta unstable validate \
+        +quit
+fi
+
+ADMINPASSWORD="${ADMINPASSWORD:-}"
+SERVERNAME="${SERVERNAME:-}"
+
+args=()
+
+if [[ -n "${ADMINPASSWORD}" ]]; then
+    args+=("-adminpassword" "${ADMINPASSWORD}")
+fi
+
+if [[ -n "${SERVERNAME}" ]]; then
+    args+=("-servername" "${SERVERNAME}")
+fi
+
+exec "$INSTALL_DIR/start-server.sh" "${args[@]}" "$@"
